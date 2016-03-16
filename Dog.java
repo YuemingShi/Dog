3efa4d0795d7e6a08a9df25a49df4fb6a57fe303
YuemingShi <l4d2alan@gmail.com>
2016-03-16 12:02:21 -0400

    Added files via upload

--- Dog.java
+++ Dog.java
@@ -10,5 +10,30 @@ public class Dog{
     this.name=name;
   }
   
+  public void speak(){
+    System.out.println("woof");
+      
+  }
   
+  
+  public static int dogYears(Dog a){
+    return a.age*7;
+  }
+  
+  public int myAge(){
+    return this.age*7;
+  }
+  
+  public static void sitDogs(Dog[] dogs){
+    for(int i=0;i<dogs.length;i++){
+      System.out.println(dogs[i].name+"! sit!");}
+  }
+  
+  public static void main(String[] args){
+    Dog tdog= new Dog("Yorkie",15,"Bea");
+    System.out.println("How old are you in dog years?");
+    for(int i=0; i<tdog.myAge();i++){
+      tdog.speak();
+    }
+  }
 }
