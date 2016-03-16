358a68f84d3fff9ed810b86555d1ac3c8c815b1a
YuemingShi <l4d2alan@gmail.com>
2016-03-16 11:18:56 -0400

    Added files via upload

---
+++ Dog.java
@@ -0,0 +1,14 @@
+public class Dog{
+  
+  String breed;
+  int age;
+  String name;
+  
+  public Dog(String breed,int age,String name){
+    this.breed=breed;
+    this.age=age;
+    this.name=name;
+  }
+  
+  
+}
